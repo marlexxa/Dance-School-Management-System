bebc985e63959e9645872162e9fe1554c516c61e
Adam Spinek <adamspinek@gmail.com>
2021-03-04 19:54:47 +0100

    feature(payment): add payment module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PaymentSchema } from './entities/payment.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Payment',
+        schema: PaymentSchema,
+      },
+    ]),
+  ],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
