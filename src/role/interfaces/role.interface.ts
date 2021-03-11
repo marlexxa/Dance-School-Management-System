6a4fce8e0a0a06e729b5d54f283a88ac05a47512
marlexxa <marlexxa@gmail.com>
2021-03-11 20:27:11 +0100

    refactor(role interface): delete readonly

--- src/role/interfaces/role.interface.ts
+++ src/role/interfaces/role.interface.ts
@@ -1,7 +1,6 @@
 import { Document } from 'mongoose';
 
 export interface RoleInterface extends Document {
-  readonly id?: string;
-  readonly role: string;
-  readonly userID: string;
+  role: string;
+  userID: string;
 }
