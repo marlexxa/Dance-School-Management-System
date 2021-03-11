c32855284bdabd0f89a70ccc1e6b8a7a96b0fdaa
marlexxa <marlexxa@gmail.com>
2021-03-11 20:23:25 +0100

    refactor(role dto): delete the body of UpdateRoleDto as it exteds CreateRoleDto

--- src/role/dto/update-role.dto.ts
+++ src/role/dto/update-role.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoleDto } from './create-role.dto';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {
-  id?: string;
-  role: string;
-  userID: string;
-}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
