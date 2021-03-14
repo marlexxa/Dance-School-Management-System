63c30afeb0996757a26388c590092e5e4612ff66
marlexxa <marlexxa@gmail.com>
2021-03-14 01:34:13 +0100

    feat(authentication): add local guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authService: AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard) //it will run local strategy before post request
+  @Post('auth/login')
+  async login(@Request() request) {
+    //return this.authService.login(request.user);
   }
 }
