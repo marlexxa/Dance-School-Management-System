5a31fa67c1b91b5c0a18216383aa8a79d0985b0b
Adam Spinek <adamspinek@gmail.com>
2021-04-12 20:50:15 +0200

    feat(core): add cors and phone method fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const options = new DocumentBuilder()
     .setTitle('Dance School')
