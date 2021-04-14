886a90093831ea7261b79ee11715082f396a8975
DariuszKny <nasz_email_do_github>
2021-04-14 21:31:04 +0200

    feat(all): update major modules

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,2 +1,2 @@
-export const mongoUri = 'mongodb://localhost:27017/dance_school';
-//export const mongoUri = 'mongodb+srv://HerokuApp:HerokuApp@cluster0.u3qcv.mongodb.net/dance_school?retryWrites=true&w=majority';
+//export const mongoUri = 'mongodb://localhost:27017/dance_school';
+export const mongoUri = 'mongodb+srv://HerokuApp:HerokuApp@cluster0.u3qcv.mongodb.net/dance_school?retryWrites=true&w=majority';
